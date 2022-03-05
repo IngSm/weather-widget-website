ef3c9e20aac0685ee4810556271d2944f0f18c8c
Alexander Gorbunov <trapicoo@gmail.com>
2022-03-05 19:09:41 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+ <div class="main">1</div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  width: 100%;
+  height: 100vh;
+  background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
+}
+
+.main {
+  height: 100%;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup>
+import weather from '@/assets/svgs/Weather-widget-icon.svg'
+</script>
+
+<template>
+<div class="header">
+    <img src="weather" alt="">
+</div>
+</template>
