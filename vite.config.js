30f6bccd1206527ac75e93a102a7490a92614c1a
Alexander Gorbunov <trapicoo@gmail.com>
2022-03-09 02:36:15 +0700

    serv

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,10 @@ import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001,
+    host: '0.0.0.0'
+  },
   resolve: {
     alias: {
       "@": `${path.resolve(__dirname, "src")}/`,
